26728ea2dc2c9e9ac7c0a132a08735ed5f57f729
prombez <prombez@gmail.com>
2022-04-03 15:16:36 +0300

    History Event -> функционал по обработке - вынесен в отдельный трейт

--- models/History.php
+++ models/History.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use app\models\interfaces\HistoryEventInterface;
+use app\models\traits\HistoryEventTrait;
 use app\models\traits\ObjectNameTrait;
 use Yii;
 use yii\db\ActiveQuery;
@@ -32,7 +33,7 @@ use yii\db\ActiveRecord;
  */
 class History extends ActiveRecord implements HistoryEventInterface
 {
-    use ObjectNameTrait;
+    use ObjectNameTrait, HistoryEventTrait;
 
     /**
      * @inheritdoc
@@ -91,48 +92,6 @@ class History extends ActiveRecord implements HistoryEventInterface
     {
         return $this->hasOne(User::class, ['id' => 'user_id']);
     }
-
-    /**
-     * @return array
-     */
-    public static function getEventTexts()
-    {
-        return [
-            self::EVENT_CREATED_TASK => Yii::t('app', 'Task created'),
-            self::EVENT_UPDATED_TASK => Yii::t('app', 'Task updated'),
-            self::EVENT_COMPLETED_TASK => Yii::t('app', 'Task completed'),
-
-            self::EVENT_INCOMING_SMS => Yii::t('app', 'Incoming message'),
-            self::EVENT_OUTGOING_SMS => Yii::t('app', 'Outgoing message'),
-
-            self::EVENT_CUSTOMER_CHANGE_TYPE => Yii::t('app', 'Type changed'),
-            self::EVENT_CUSTOMER_CHANGE_QUALITY => Yii::t('app', 'Property changed'),
-
-            self::EVENT_OUTGOING_CALL => Yii::t('app', 'Outgoing call'),
-            self::EVENT_INCOMING_CALL => Yii::t('app', 'Incoming call'),
-
-            self::EVENT_INCOMING_FAX => Yii::t('app', 'Incoming fax'),
-            self::EVENT_OUTGOING_FAX => Yii::t('app', 'Outgoing fax'),
-        ];
-    }
-
-    /**
-     * @param $event
-     * @return mixed
-     */
-    public static function getEventTextByEvent($event)
-    {
-        return static::getEventTexts()[$event] ?? $event;
-    }
-
-    /**
-     * @return mixed|string
-     */
-    public function getEventText()
-    {
-        return static::getEventTextByEvent($this->event);
-    }
-
 
     /**
      * @param $attribute
