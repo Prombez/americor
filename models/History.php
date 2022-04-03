67215073a6ba3594c12a6ec398ba6061bbcba691
prombez <prombez@gmail.com>
2022-04-03 15:06:36 +0300

    History Event -> вынесены в отдельный интерфейс

--- models/History.php
+++ models/History.php
@@ -2,6 +2,7 @@
 
 namespace app\models;
 
+use app\models\interfaces\HistoryEventInterface;
 use app\models\traits\ObjectNameTrait;
 use Yii;
 use yii\db\ActiveQuery;
@@ -29,25 +30,9 @@ use yii\db\ActiveRecord;
  * @property Sms $sms
  * @property Call $call
  */
-class History extends ActiveRecord
+class History extends ActiveRecord implements HistoryEventInterface
 {
     use ObjectNameTrait;
-
-    const EVENT_CREATED_TASK = 'created_task';
-    const EVENT_UPDATED_TASK = 'updated_task';
-    const EVENT_COMPLETED_TASK = 'completed_task';
-
-    const EVENT_INCOMING_SMS = 'incoming_sms';
-    const EVENT_OUTGOING_SMS = 'outgoing_sms';
-
-    const EVENT_INCOMING_CALL = 'incoming_call';
-    const EVENT_OUTGOING_CALL = 'outgoing_call';
-
-    const EVENT_INCOMING_FAX = 'incoming_fax';
-    const EVENT_OUTGOING_FAX = 'outgoing_fax';
-
-    const EVENT_CUSTOMER_CHANGE_TYPE = 'customer_change_type';
-    const EVENT_CUSTOMER_CHANGE_QUALITY = 'customer_change_quality';
 
     /**
      * @inheritdoc
---
+++ models/interfaces/HistoryEventInterface.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\models\interfaces;
+
+interface HistoryEventInterface
+{
+    const EVENT_CREATED_TASK = 'created_task';
+    const EVENT_UPDATED_TASK = 'updated_task';
+    const EVENT_COMPLETED_TASK = 'completed_task';
+
+    const EVENT_INCOMING_SMS = 'incoming_sms';
+    const EVENT_OUTGOING_SMS = 'outgoing_sms';
+
+    const EVENT_INCOMING_CALL = 'incoming_call';
+    const EVENT_OUTGOING_CALL = 'outgoing_call';
+
+    const EVENT_INCOMING_FAX = 'incoming_fax';
+    const EVENT_OUTGOING_FAX = 'outgoing_fax';
+
+    const EVENT_CUSTOMER_CHANGE_TYPE = 'customer_change_type';
+    const EVENT_CUSTOMER_CHANGE_QUALITY = 'customer_change_quality';
+}
